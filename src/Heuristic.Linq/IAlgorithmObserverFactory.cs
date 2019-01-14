786047044def789fe3e38a0a7b1ac5948e730e10
Robert Huang <robert.h@btigroup.io>
2019-01-14 14:46:58 +0800

    Adjust IAlgorithmObserverFactory design

--- src/Heuristic.Linq/IAlgorithmObserverFactory.cs
+++ src/Heuristic.Linq/IAlgorithmObserverFactory.cs
@@ -7,13 +7,14 @@ namespace Heuristic.Linq
     /// <summary>
     /// Represents the object that is able to create <see cref="IProgress{T}"/> instances.
     /// </summary>
-    public interface IAlgorithmObserverFactory
+    /// <typeparam name="TStep">The type of step of the problem.</typeparam>
+    public interface IAlgorithmObserverFactory<TStep>
     {
         /// <summary>
         /// Creates an <see cref="IProgress{T}"/> instance where T is <see cref="AlgorithmState{TFactor, TStep}"/>.
         /// </summary>
         /// <param name="source">The instance that allows LINQ expressions to be applied to.</param>
         /// <returns>An <see cref="IProgress{T}"/> instance.</returns>
-        IProgress<AlgorithmState<TFactor, TStep>> Create<TFactor, TStep>(HeuristicSearchBase<TFactor, TStep> source);
+        IProgress<AlgorithmState<TFactor, TStep>> Create<TFactor>(HeuristicSearchBase<TFactor, TStep> source);
     }
 }
