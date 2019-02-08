1fe57f872e84a375a5b06a46ed9a21585b8c03d4
Robert Vandenberg Huang <rvh.omni@gmail.com>
2019-02-08 10:00:08 +0800

    Improve usability of default implementation

--- src/Heuristic.Linq/AlgorithmObserverFactory.cs
+++ src/Heuristic.Linq/AlgorithmObserverFactory.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Heuristic.Linq
 {
@@ -6,10 +7,17 @@ namespace Heuristic.Linq
 
     /// <summary>
     /// Provides the default implementation of <see cref="IAlgorithmObserverFactory{TStep}"/> interface.
-    /// </summary>
-    /// <typeparam name="TStep"></typeparam>
+    /// </summary> 
+    /// <typeparam name="TStep">The type of step of the problem.</typeparam>   
     public class AlgorithmObserverFactory<TStep> : IAlgorithmObserverFactory<TStep>
     {
+        private readonly List<AlgorithmProgress<TStep>> progresses;
+
+        /// <summary>
+        /// Gets the collection of <see cref="AlgorithmProgress{TStep}"/> instances created by the factory.
+        /// </summary>
+        public IReadOnlyList<AlgorithmProgress<TStep>> Progresses => progresses;
+
         /// <summary>
         /// Invoked when a new <see cref="AlgorithmProgress{TStep}"/> instance is created.
         /// </summary>
@@ -18,7 +26,7 @@ namespace Heuristic.Linq
         /// <summary>
         /// Initializes a new instance of current class.
         /// </summary>
-        public AlgorithmObserverFactory() { }
+        public AlgorithmObserverFactory() => progresses = new List<AlgorithmProgress<TStep>>();
 
         IProgress<AlgorithmState<TFactor, TStep>> IAlgorithmObserverFactory<TStep>.Create<TFactor>(HeuristicSearchBase<TFactor, TStep> source)
         {
@@ -35,6 +43,7 @@ namespace Heuristic.Linq
         /// <param name="progress">The new <see cref="AlgorithmProgress{TStep}"/> instance.</param>
         protected virtual void OnCreated(AlgorithmProgress<TStep> progress)
         {
+            progresses.Add(progress);
             Created?.Invoke(this, progress);
         }
     }
